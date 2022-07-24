46486ef74a4ecf01906a6e6658727ab6dfac7332
serverwizard <serverwizard@naver.com>
2022-07-25 01:09:12 +0900

    feat: [step2] 계산기 프로그램 웹 애플리케이션으로 만들기

---
+++ src/main/java/org/example/ClientRequestHandler.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.example.calculator.domain.Calculator;
+import org.example.calculator.domain.PositiveNumber;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class ClientRequestHandler implements Runnable {
+    private static final Logger logger = LoggerFactory.getLogger(ClientRequestHandler.class);
+
+    private final Socket clientSocket;
+
+    public ClientRequestHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        logger.info("[ClientRequestHandler] new client {} started.", Thread.currentThread().getName());
+        try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            DataOutputStream dos = new DataOutputStream(out);
+
+            HttpRequest httpRequest = new HttpRequest(br);
+
+            // GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
+            if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
+                QueryStrings queryStrings = httpRequest.getQueryStrings();
+
+                int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+                String operator = queryStrings.getValue("operator");
+                int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+
+                int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                byte[] body = String.valueOf(result).getBytes();
+
+                HttpResponse response = new HttpResponse(dos);
+                response.response200Header("application/json", body.length);
+                response.responseBody(body);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
--- src/main/java/org/example/CustomWebApplicationServer.java
+++ src/main/java/org/example/CustomWebApplicationServer.java
@@ -30,30 +30,9 @@ public class CustomWebApplicationServer {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
                 /**
-                 * Step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
+                 * Step2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
                  */
-                try (InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    // GET /calculate?operand1=11&operator=*&operand2=55 HTTP/1.1
-                    if (httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-                }
+                new Thread(new ClientRequestHandler(clientSocket)).start();
             }
         }
     }
